ed991f52303e2431aff5f2067190b5d0bd30f147
michal-wolski <michal.wolski130@gmail.com>
2022-05-29 00:15:03 +0200

    local

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,22 @@ class App extends Component {
     contacts: [],
     filter: '',
   };
+  //Download phonebook from user localstorage
+  componentDidMount() {
+    try {
+      const phonebookParse = localStorage.getItem('phonebook');
+      if (phonebookParse) {
+        const contacts = JSON.parse(phonebookParse);
+        this.setState({ contacts });
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('phonebook', JSON.stringify(this.state.contacts));
+  }
 
   inputIds = {
     nameId: nanoid(),
